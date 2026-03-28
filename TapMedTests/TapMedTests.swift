2ac4d01d55e1dce5aabe4f774a4a799fd8842c67
tuskedtooth <tuskedtooth3521@gmail.com>
2026-03-28 14:14:36 -0400

    first files

---
+++ TapMedTests/TapMedTests.swift
@@ -0,0 +1,19 @@
+//
+//  TapMedTests.swift
+//  TapMedTests
+//
+//  Created by Adam on 3/28/26.
+//
+
+import Testing
+@testable import TapMed
+
+struct TapMedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
